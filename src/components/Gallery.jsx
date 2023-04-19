fc50e6edc1cc60dc3fac5145c886b3daa4019678
Jirawat Rongsupan <74420545+LorZangel@users.noreply.github.com>
2023-04-19 23:12:50 +0700

    Add files via upload

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,12 @@
+function Gallery(){
+    return(
+        <section className="Gsite">
+            <h2>Gallery</h2>
+            <ul>
+                <li><img width={200} src="src\assets\me.jpg" alt="picture"></img></li>
+                <li><img width={200} src="src\assets\me.jpg" alt="picture"></img></li>
+                <li><img width={200} src="src\assets\me.jpg" alt="picture"></img></li>
+            </ul>
+        </section>
+    );
+} export default Gallery;
